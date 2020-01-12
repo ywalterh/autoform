5170bad328e3d7ef21de77bc19b6dcba772e5d84
Walter <waltermwfs@gmail.com>
2020-01-12 16:52:17 -0500

    Unzip and showed odf files

--- README.md
+++ README.md
@@ -7,4 +7,10 @@ Use AI to auto format slides based on existing odf slides
  * [ ] Use code to interact with the styling of the elements, notably the position of them
  * [ ] Use a randomizing algorithm to style the slides 
 
+
+
+## References
+* How to avoid unwrap (https://dmerej.info/blog/post/killing-unwrap/)
+* How to parse ODF file using python as example (https://www.linuxjournal.com/article/9347)
+
 ## Open Issues
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
+use std::path::Path;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    // interesting patterns to manipulate filename
+    std::process::exit(real_main());
+}
+
+
+fn real_main() -> i32 {
+    // Parse input arguments
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", args[0]);
+        return 1;
+    }
+
+    let file_name = Path::new(&*args[1]);
+    println!("Parsing {}", file_name.display());
+    match unzip_odf(file_name) {
+        Err(err)  => {
+            println!("Error: {}", err);
+            return 1;
+        }
+        Ok(()) => ()
+    };
+    return 0;
+}
+
+fn unzip_odf(file_name: &Path) -> Result<(), Box<dyn Error>>{
+    let file = fs::File::open(file_name)?;
+    let mut archive = zip::ZipArchive::new(file)?;
+
+    for i in 0..archive.len() {
+        let file = archive.by_index(i).unwrap();
+        println!("File #{} : {}", i,  file.sanitized_name().as_path().display());
+    }
+
+    return Ok(());
 }
