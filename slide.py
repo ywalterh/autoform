54612341d46451abec2ad1388897840b9082a070
Walter <waltermwfs@gmail.com>
2020-01-12 15:47:51 -0500

    Moved to rust repo

--- slide.py
+++ slide.py
@@ -1,14 +1,12 @@
 import sys
-from odf.opendocument import load 
+from odf.opendocument import load
 
 infile = sys.argv[1]
 
 doc = load(infile)
-
 for key in doc.element_dict:
     for x in key:
-        print(x)
+        print(str(x))
     for elm in doc.element_dict[key]:
-        print(elm)
+        print(elm.qname)
 
-    print("-------------------")
