7f3a805bbd8f6494e2127e820c44487b5d60abee
walterh <waltermwfs@gmail.com>
2020-01-19 13:59:45 -0500

    Revert it back to having 1-demensional array for y and output. added sigmoid_derivative for 1Darray

--- src/nn.rs
+++ src/nn.rs
@@ -10,8 +10,8 @@ pub struct Network {
     weights1: Array2<f32>,
     weights2: Array2<f32>,
     //@Next step, makes this work on multi-dimensional
-    y: f32,
-    output: f32,
+    y: Array1<f32>,
+    output: Array1<f32>,
     layer1: Array1<f32>,
     layer2: Array1<f32>,
 }
@@ -23,9 +23,10 @@ mod tests {
     #[test]
     fn test_new() {
         let x = Array1::<f32>::zeros(3);
-        let mut result = Network::new(x, 0_f32);
+        let y = Array1::<f32>::zeros(1);
+        let mut result = Network::new(x, y);
 
-        assert_eq!(result.output, 0_f32);
+        assert_eq!(result.output.len(), 1);
 
         // in reality, this is a 3x4 array, so len is 12
         assert_eq!(result.weights1.len(), 12);
@@ -40,17 +41,24 @@ mod tests {
     fn test_sigmoid_derivative() {
         assert_ne!(Network::sigmoid_derivative(1_f32), 0_f32);
     }
+
+    #[test]
+    fn test_array1_sigmoid_derivative() {
+        let input_x = Array1::<f32>::ones(1);
+        let result = Network::array1_sigmoid_derivative(&input_x);
+        assert_eq!(result.len(), 1);
+        assert_ne!(result[0], 1_f32);
+    }
 }
 
 // Initialize the NN with empty layers
 // and input outpu as arrrys?
 // Having some troubele translating things along
 impl Network {
-    fn new(x: Array1<f32>, y: f32) -> Network {
+    fn new(x: Array1<f32>, y: Array1<f32>) -> Network {
         let mut rng = rand::thread_rng();
         let mut weights1 = Array::zeros((x.len(), 4));
-        let mut weights2 = Array::zeros((4, 1));
-
+        let mut weights2 = Array::zeros((4, 1)); 
         // iterate weights to initialize it with random numbers
         for mut row in weights1.genrows_mut() {
             row.fill(rng.gen_range(0_f32, 1_f32));
@@ -61,13 +69,13 @@ impl Network {
 
         // explicitly make a copy of the value of len of y
         // not usd yet, but let's change it to multi-dimensional later
-        //let y_shape = y.len();
+        let y_shape = y.len();
         Network {
             input: x,
             weights1,
             weights2,
             y,
-            output: 0_f32, 
+            output: Array1::zeros(y_shape), 
             // do we need to pre-assign values?
             // guess it doesn't matter because we'll use 
             // them later anyway
@@ -90,10 +98,30 @@ impl Network {
 
     fn back_propagation(&mut self) {
         // application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        //let d_weights2 = self.layer1.dot(2_f32 * (self.y - self.output) * sigmoid_derivative(self.output));
-        //let d_weights1 = self.input.dot()
+        //let d_weights2 = self.layer1.dot(&(2_f32 * (self.y[0] - self.output[0]) * Network::sigmoid_derivative(self.output[0])));
 
+//d_weights1 = np.dot(self.input.T,  np.dot(2*((self.y - self.output * sigmoid_derivative)self.output(, self.weights2.T) * sigmoid_derivative)self.layer1()))
+//
+        //let d_weights1 = self.input.dot(, self.y - self.output)
+    }
 
+    // borrow the value to perform sigmoid derivative to one
+    // one dimensional array and return the resulting array
+    fn array1_sigmoid_derivative(x: &Array1<f32>) -> Array1<f32>{
+        let mut result = Array1::<f32>::zeros(x.len());
+        for  row in result.genrows_mut() {
+            // iterate over row to add data
+            let mut index = 0;
+
+            // this should be correct since it's already filled, 
+            // the recommended approach is to use .fill
+            // which obviously doesn't work for us here
+            for i in row {
+                *i = Network::sigmoid_derivative(x[index]);
+                index += 1;
+            }
+        } 
+        return result;
     }
 
     fn sigmoid_derivative(x: f32) -> f32 {
