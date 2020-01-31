e510a03d22a965581490206e1a74bc6009dd714f
Walter Huang <waltermwfs@gmail.com>
2020-01-30 22:53:48 -0500

    Remove dead code warnings for now before we can integrate nn into auto formatter. Fmtting..

--- src/nn.rs
+++ src/nn.rs
@@ -1,26 +1,28 @@
 extern crate ndarray;
 
-use ndarray::{Array2};
+use ndarray::Array2;
 use rand::prelude::*;
 
 #[cfg(test)]
 mod tests {
-    use ndarray::{arr2};
     use super::*;
+    use ndarray::arr2;
 
     #[test]
     fn test_new() {
-        let x = arr2(&[[0_f32, 0_f32, 1_f32],
-                         [0_f32, 1_f32, 1_f32],
-                         [1_f32, 0_f32, 1_f32],
-                         [1_f32, 1_f32, 1_f32]]);
+        let x = arr2(&[
+            [0_f32, 0_f32, 1_f32],
+            [0_f32, 1_f32, 1_f32],
+            [1_f32, 0_f32, 1_f32],
+            [1_f32, 1_f32, 1_f32],
+        ]);
 
         let y = arr2(&[[0_f32], [1_f32], [1_f32], [0_f32]]);
         let mut result = Network::new(x, y);
 
         assert_eq!(result.output.shape()[0], 4);
 
-        // in reality, this is a 3x4 array, so len is 12. lossy way of testing it 
+        // in reality, this is a 3x4 array, so len is 12. lossy way of testing it
         assert_eq!(result.weights1.len(), 12);
         assert_ne!(result.weights1[[0, 0]], result.weights1[[0, 1]]);
 
@@ -30,9 +32,9 @@ mod tests {
         // see if feedforward is working
         assert_ne!(result.layer1[[0, 0]], 0_f32);
 
-        let before_propagate = result.weights2[[0,0]];
+        let before_propagate = result.weights2[[0, 0]];
         result.back_propagation();
-        assert_ne!(result.weights2[[0,0]], before_propagate);
+        assert_ne!(result.weights2[[0, 0]], before_propagate);
     }
 
     #[test]
@@ -42,10 +44,12 @@ mod tests {
         //1, 0, 1 -> 1,
         //1, 1, 1 -> 0
         // This is simulating a three features input with a single output
-        let x = arr2(&[[0_f32, 0_f32, 1_f32],
-                         [0_f32, 1_f32, 1_f32],
-                         [1_f32, 0_f32, 1_f32],
-                         [1_f32, 1_f32, 1_f32]]);
+        let x = arr2(&[
+            [0_f32, 0_f32, 1_f32],
+            [0_f32, 1_f32, 1_f32],
+            [1_f32, 0_f32, 1_f32],
+            [1_f32, 1_f32, 1_f32],
+        ]);
 
         let y = arr2(&[[0_f32], [1_f32], [1_f32], [0_f32]]);
 
@@ -53,16 +57,14 @@ mod tests {
 
         for _ in 1..2500 {
             network.feed_forward();
-            network.back_propagation(); 
+            network.back_propagation();
         }
-        
         println!("Actual:\n {}", network.y);
         println!("Predication:\n {}", network.output);
 
         // calculate means to make sure it's smaller than 0.001
         let cost_square = (network.y - network.output).mapv(square);
         let mean = cost_square.sum() / cost_square.len() as f32;
-        
         println!("Loss is :\n {}", mean);
         assert!(mean < 0.001_f32);
     }
@@ -97,6 +99,7 @@ pub struct Network {
 // and input outpu as arrrys?
 // Having some troubele translating things along
 impl Network {
+    #[allow(dead_code)]
     fn new(x: Array2<f32>, y: Array2<f32>) -> Network {
         let mut rng = rand::thread_rng();
         // use the shape of x to determine what the shape of weights1 has
@@ -132,28 +135,33 @@ impl Network {
         }
     }
 
+    #[allow(dead_code)]
     fn feed_forward(&mut self) {
         // add the result each iteration  to output
         self.layer1 = self.input.dot(&self.weights1).mapv_into(Network::sigmoid);
         self.output = self.layer1.dot(&self.weights2).mapv_into(Network::sigmoid);
     }
 
+    #[allow(dead_code)]
     fn back_propagation(&mut self) {
-        // application of the chain rule to find derivative of 
+        // application of the chain rule to find derivative of
         // the loss function with respect to weights2 and weights1
         let z = 2_f32 * (&self.y - &self.output) * &self.output.mapv(Network::sigmoid_derivative);
         let d_weights2 = self.layer1.t().dot(&z);
-        let d_weights1 = self.input.t().dot(&(z.dot(&self.weights2.t()) * &self.layer1.mapv(Network::sigmoid_derivative)));
-       
+        let d_weights1 = self
+            .input
+            .t()
+            .dot(&(z.dot(&self.weights2.t()) * &self.layer1.mapv(Network::sigmoid_derivative)));
         self.weights1 = &self.weights1 + &d_weights1;
         self.weights2 = &self.weights2 + &d_weights2;
     }
 
-
+    #[allow(dead_code)]
     fn sigmoid(x: f32) -> f32 {
         1_f32 / (1_f32 + (-x).exp())
     }
 
+    #[allow(dead_code)]
     fn sigmoid_derivative(x: f32) -> f32 {
         x * (1_f32 - x)
     }
