6a000035b8bde6d49a33730ec1d43f88a6a5d780
Walter <waltermwfs@gmail.com>
2020-01-31 19:53:34 -0500

    Migrate to xml-rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 extern crate rand;
 
-use quick_xml::events::{BytesEnd, BytesStart, Event};
-use quick_xml::Reader;
-use quick_xml::Writer;
 use rand::Rng;
 use std::borrow::Cow;
 use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
-use std::io::Cursor;
 use std::path::Path;
-use std::str::from_utf8;
+use xml::attribute::{Attribute, OwnedAttribute};
+use xml::name::OwnedName;
+use xml::reader::{EventReader, XmlEvent};
+use xml::writer::EmitterConfig;
+use xml::writer::XmlEvent as wXmlEvent;
 use zip::write::FileOptions;
 
 // import neural network module
@@ -59,7 +59,7 @@ fn unzip_odf(file_name: &Path) -> Result<(), Box<dyn Error>> {
             let mut xml_content_buffer = String::new();
             file.read_to_string(&mut xml_content_buffer)?;
             zip.start_file("content.xml", options)?;
-            zip.write_all(&update_content_xml(&xml_content_buffer))?;
+            zip.write_all(&update_content_xml(&xml_content_buffer)?)?;
         } else {
             let mut buf = Vec::new();
             file.read_to_end(&mut buf)?;
@@ -75,78 +75,95 @@ fn unzip_odf(file_name: &Path) -> Result<(), Box<dyn Error>> {
 // Insead of doing in place update a better approach is to build
 // data binding for each document format and then update it to
 // save into odp files
-fn update_content_xml(xml_content_buffer: &str) -> std::vec::Vec<u8> {
+fn update_content_xml(xml_content_buffer: &str) -> Result<std::vec::Vec<u8>, Box<dyn Error>> {
     let mut rng = rand::thread_rng();
-
-    let mut reader = Reader::from_str(xml_content_buffer);
-    reader.trim_text(true);
-
-    let mut writer = Writer::new(Cursor::new(Vec::new()));
-
-    //let mut txt = Vec::new();
-    let mut buf = Vec::new();
-
-    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
-    loop {
-        // let's match attributes with svg: ... and then modify it in place
-        // hopefully it's doable
-        match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-                // Let's copy it for now, not sure if it's feasible to
-                // do in place update
-                let mut elem = BytesStart::owned(e.name(), e.name().len());
-                let element_name = from_utf8(e.name()).unwrap();
-
-                // push existing elem along, but perform inplace update
-                // if attribute contains svg: like settings
-                // assuming them to be cm for now
-                //let mut class_name = "N/A";
-
-                let attributes_clone = e.attributes().clone();
-                attributes_clone.for_each(|attr| {
-                    let attr_found = attr.unwrap();
-                    if from_utf8(attr_found.key)
-                        .unwrap_or("")
-                        .contains("presentation:class")
-                    {
-                        let tmp_value = &attr_found.unescaped_value().unwrap();
-                        let class_name = from_utf8(tmp_value).unwrap();
-                        println!("Class name is {}", class_name);
-                    }
-                });
-
-                elem.extend_attributes(e.attributes().map(|attr| {
-                    //@Cleanup remove unwrap
-                    let mut attribute = attr.unwrap();
-                    match from_utf8(attribute.key) {
-                        Ok(key) => {
-                            if key.starts_with("svg:") {
-                                let random_s: String = format!("{}cm", rng.gen_range(1, 10));
-                                println!("Setting {} : {} to {}", element_name, key, random_s);
-                                attribute.value = Cow::Owned(random_s.into_bytes());
-                            }
-                        }
-                        Err(e) => println!("{}", e),
-                    }
-
-                    attribute
-                }));
-
-                assert!(writer.write_event(Event::Start(elem)).is_ok());
+    let parser = EventReader::from_str(xml_content_buffer);
+    let mut target: Vec<XmlEvent> = Vec::new();
+    for e in parser {
+        match e {
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
             }
-            Ok(Event::End(ref e)) => {
-                assert!(writer
-                    .write_event(Event::End(BytesEnd::borrowed(e.name())))
-                    .is_ok());
-            }
-            Ok(Event::Eof) => break, // exits the loop when reaching end of file
-            Ok(e) => assert!(writer.write_event(&e).is_ok()),
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            // grab the rest
+            elem => match elem {
+                Ok(elem_real) => {
+                    target.push(elem_real);
+                }
+                Err(_) => {}
+            },
         }
-
-        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
-        buf.clear();
     }
 
-    return writer.into_inner().into_inner();
+    let mut content_target: Vec<u8> = Vec::new();
+    let mut writer = EmitterConfig::new()
+        .perform_indent(true)
+        .create_writer(&mut content_target);
+
+    //@Cleanup setup result to handle Err
+    for t in target {
+        match t {
+            XmlEvent::StartElement {
+                name,
+                attributes,
+                namespace,
+            } => {
+                // Randomize attribute with svg
+                let mut attrs: Vec<Attribute> = Vec::new();
+                for attribute in &attributes {
+                    // copy the value
+                    let mut copy_actual_value: String = String::from(&attribute.value);
+
+                    // setup prefix
+                    match &attribute.name.prefix {
+                        Some(prefix) => {
+                            if prefix == "svg" {
+                                if copy_actual_value.ends_with("cm") {
+                                    let random_s = format!("{}cm", rng.gen_range(1, 10));
+                                    copy_actual_value = random_s;
+                                }
+                            }
+                        }
+                        None => {}
+                    }
+
+                    attrs.push(Attribute::new(attribute.name.borrow(), &copy_actual_value));
+                }
+
+                writer.write(wXmlEvent::StartElement {
+                    name: name.borrow(),
+                    attributes: Cow::Owned(attrs),
+                    namespace: Cow::Borrowed(&namespace),
+                })?;
+            }
+            XmlEvent::EndElement { name } => {
+                writer.write(wXmlEvent::EndElement {
+                    name: Some(name.borrow()),
+                })?;
+            }
+            XmlEvent::StartDocument {
+                version,
+                encoding,
+                standalone,
+            } => {
+                writer.write(wXmlEvent::StartDocument {
+                    version,
+                    encoding: Some(&encoding),
+                    standalone,
+                })?;
+            }
+            XmlEvent::Characters(c) => {
+                writer.write(wXmlEvent::Characters(&c))?;
+            }
+            XmlEvent::Comment(c) => {
+                writer.write(wXmlEvent::Comment(&c))?;
+            }
+            XmlEvent::CData(c) => {
+                writer.write(wXmlEvent::CData(&c))?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(content_target)
 }
